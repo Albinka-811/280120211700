a4e9a8233712cca30a254fc07331264e8840b052
Albina Holovach <albinkasolodenko@gmail.com>
2021-01-31 19:45:57 +0200

    Implement user-info block

---
+++ src/layouts/user-info/user-info.js
@@ -0,0 +1,17 @@
+// @vue/component
+export default {
+    name: 'user-info',
+    data () {
+        return {
+            user: {
+                photo: require('@assets/photo.png'),
+                name: 'Вероника Ростова',
+                speciality: 'Менеджер по продажам',
+                message: 'Подберу для вас самые лучшие предложения. Мои услуги абсолютно бесплатны',
+            },
+
+        }
+    },
+    computed: {},
+    methods: {},
+}
---
+++ src/layouts/user-info/user-info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="user">
+        <div class="user-avatar">
+            <img class="user-avatar__photo" :src="user.photo" alt="Аватарка">
+        </div>
+
+        <div class="user-info">
+            <h1 class="user-info__name">{{ user.name }}</h1>
+            <h3 class="user-info__speciality"> {{ user.speciality }} </h3>
+
+            <p class="user-info__message"> {{ user.message }} </p>
+        </div>
+    </div>
+</template>
+
+<script src="./user-info.js" lang="js"></script>
+<style src="./user-info.scss" lang="scss" scoped></style>
